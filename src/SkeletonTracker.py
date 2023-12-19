fdaece2e550b1b87341f5e5beef206e3ac15ce0d
CARI JRL <cari@jrl.com>
2023-12-19 17:49:18 +0100

    fixed display and camera launch. WIP: skeleton_tracker

--- src/SkeletonTracker.py
+++ src/SkeletonTracker.py
@@ -35,39 +35,47 @@ class SkeletonTracker:
     def set_init_params(self):
         self.init_params = sl.InitParameters()
 
-        self.init_params.camera_resolution = read_param('camera_resolution',
-                                                        default_value=sl.RESOLUTION.HD1080)
-        self.init_params.depth_mode = read_param('depth_mode',
-                                                 default_value=sl.DEPTH_MODE.PERFORMANCE)
-        self.init_params.coordinate_units = read_param('unit',
-                                                       default_value=sl.UNIT.METER)
-        self.init_params.sdk_verbose = read_param('sdk_verbose',
+        self.init_params.camera_resolution = sl.RESOLUTION[read_param('camera_params/camera_resolution',
+                                                                      default_value='HD1080')]
+
+        self.init_params.depth_mode = sl.DEPTH_MODE[read_param('camera_params/depth_mode',
+                                                               default_value='PERFORMANCE')]
+        self.init_params.coordinate_units = sl.UNIT[read_param('camera_params/unit',
+                                                               default_value='METER')]
+        self.init_params.sdk_verbose = read_param('camera_params/sdk_verbose',
                                                   default_value=1)
         
-        err = self.camera.open(self.init_params)
-        if err != sl.ERROR_CODE.SUCCESS:
-            rospy.logerr("Camera Open : "+repr(err)+". Exit program.")
-            exit()
+        rospy.loginfo("Camera Open: Loading Module...")
+        if not self.camera.is_opened():
+            
+            err = self.camera.open(self.init_params)
+            if err != sl.ERROR_CODE.SUCCESS:
+                rospy.logerr("Camera Open : "+repr(err)+". Exit program.")
+                exit()
+        else:
+            rospy.loginfo("Camera Open: Already opened")
 
 
     def set_body_params(self):
         self.body_params = sl.BodyTrackingParameters()
 
-        self.body_params.detection_model = read_param('detection_model',
-                                                      default_value=sl.BODY_TRACKING_MODEL.HUMAN_BODY_FAST) # optimize the runtime or the accuracy
-        self.body_params.enable_tracking = read_param('enable_tracking',
+        self.body_params.detection_model = sl.BODY_TRACKING_MODEL[read_param('tracking_params/detection_model',
+                                                                             default_value='HUMAN_BODY_FAST')] # optimize the runtime or the accuracy
+        self.body_params.enable_tracking = read_param('tracking_params/enable_tracking',
                                                       default_value=True)    
-        self.body_params.image_sync = read_param('image_sync',
+        self.body_params.image_sync = read_param('tracking_params/image_sync',
                                                  default_value=True)
-        self.body_params.enable_segmentation = read_param('enable_segmentation',
+        self.body_params.enable_segmentation = read_param('tracking_params/enable_segmentation',
                                                           default_value=False)
-        self.body_params.enable_body_fitting = read_param('enable_body_fitting',
+        self.body_params.enable_body_fitting = read_param('tracking_params/enable_body_fitting',
                                                           default_value=True) # optimize the person joints position, requires more computations
         
         if self.body_params.enable_tracking:
             self.positional_tracking_param = sl.PositionalTrackingParameters()
-            self.positional_tracking_param.set_as_static = True # enable if the camera is static for better performance
-            self.positional_tracking_param.set_floor_as_origin = True # enable if the camera is static and the floor is the origin of the world frame
+            self.positional_tracking_param.set_as_static = read_param('tracking_params/set_as_static',
+                                                                      default_value=False) # enable if the camera is static for better performance
+            self.positional_tracking_param.set_floor_as_origin = read_param('tracking_params/set_floor_as_origin',
+                                                                            default_value=True) # enable if the camera is static and the floor is the origin of the world frame
             self.camera.enable_positional_tracking(self.positional_tracking_param) # enable positional tracking
 
         rospy.loginfo("Body tracking: Loading Module...")
@@ -84,7 +92,7 @@ class SkeletonTracker:
         # For outdoor scene or long range, the confidence should be lowered to avoid missing detections (~20-30)
         # For indoor scene or closer range, a higher confidence limits the risk of false positives and increase the precision (~50+)
         self.body_runtime_params.detection_confidence_threshold = read_param('detection_confidence_threshold',
-                                                                            default_value=40)
+                                                                             default_value=40)
 
 
     def close_camera(self):
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,7 @@
 import rospy
 
-def read_param(param_name):
-    param_value = rospy.get_param(param_name)
+def read_param(param_name, default_value=None):
+    param_value = rospy.get_param(param_name, default_value)
     if param_value is None:
         rospy.logerr(f"Failed to read {param_name} from the parameter server.")
         return None
