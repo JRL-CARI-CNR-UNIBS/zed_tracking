674e2bd1c8ef754f8a5e187f39fbcefd8d3f0487
CARI JRL <cari@jrl.com>
2024-01-29 10:13:03 +0100

    completed Dockerfile: ROS2, NVIDIA CUDA, ZED, ROS1, ROS1_bridge

--- docker/ros_entrypoint.sh
+++ docker/ros_entrypoint.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 set -e
 
-# # setup ros2 environment
-# source "/opt/ros/$ROS_DISTRO/setup.bash" --
-# source "/root/perception_ws/install/local_setup.bash" --
+# Setup ROS1 workspaces
+source /root/rosbridge_ws/install/local_setup.bash
+
+# Setup ROS2 workspaces
+source /root/perception_ws/install/local_setup.bash
 
 # Welcome information
-echo "ZED ROS2 Docker Image"
 echo "---------------------"
-# echo 'ROS distro: ' $ROS_DISTRO
-# echo 'DDS middleware: ' $RMW_IMPLEMENTATION
-# echo 'ROS 2 Workspaces:' $COLCON_PREFIX_PATH
-# echo 'ROS 2 Domain ID:' $ROS_DOMAIN_ID
-# echo 'Machine IPs:' $ROS_IP
-# echo "---"  
+echo "ZED ROS2 ROS1_bridge Docker Image"
+echo "---------------------"
+
+unset ROS_DISTRO
+source $ROS1_INSTALL_PATH/setup.bash
+echo 'ROS1 distro: ' $ROS_DISTRO
+unset ROS_DISTRO
+
+source $ROS2_INSTALL_PATH/setup.bash
+echo 'ROS2 distro: ' $ROS_DISTRO
+
+echo 'ROS 1 Workspaces:' $ROS_PACKAGE_PATH
+echo 'ROS 2 Workspaces:' $COLCON_PREFIX_PATH
+
+echo 'ROS master URI:' $ROS_MASTER_URI
+
+echo "---------------------" 
 echo 'Available ZED packages:'
 ros2 pkg list | grep zed
 echo "---------------------"    
